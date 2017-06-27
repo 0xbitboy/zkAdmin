fc0a30220ba94e87eed33a722ff8d120e70403b5
liaojiacan <liaojiacan@qq.com>
2017-06-27 16:51:09 +0800

    创建工程

---
+++ src/main/java/club/calabashbrothers/zkadmin/ZkAdminApplication.java
@@ -0,0 +1,12 @@
+package club.calabashbrothers.zkadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZkAdminApplication.class, args);
+	}
+}
---
+++ src/test/java/club/calabashbrothers/zkadmin/ZkAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package club.calabashbrothers.zkadmin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZkAdminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
